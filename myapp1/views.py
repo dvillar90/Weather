88ef34e7a01596a1f9862fd0a09ac113cac7f820
Daniel Villar <daniel.villar@picuscapital.onmicrosoft.com>
2023-01-18 21:48:48 -0500

    Initial commit

---
+++ myapp1/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def view_weather(request):
+    data = dict()
+    import datetime
+    time = datetime.datetime.now()
+    xy = 1000
+    data["time_of_day"] = time
+    data['xy'] = xy
+
+    return render(request, "weather.html", context=data)
