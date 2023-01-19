65934f6c138836ce7464f00326cb292045007962
DanielVillar <villar_daniel@hotmail.com>
2023-01-18 22:40:00 -0500

    New

--- myapp1/views.py
+++ myapp1/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def view_weather(request):
     data = dict()
-    w1 = WeatherApp(city="New York", country="USA", weather=10.0)
-    c_list= w1
-    data['weather']=c_list
+    import datetime
+    time = datetime.datetime.now()
+    xy = 1000
+    data["time_of_day"] = time
+    data['xy'] = xy
 
     return render(request, "weather.html", context=data)
